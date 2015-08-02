550ed2795c60f63c1e683774aeb997cdcd755767
Danielwhyte <danwhy@gmail.com>
2015-08-02 22:01:23 +0100

    cells can be created

---
+++ conway.js
@@ -0,0 +1,59 @@
+var conway = {
+  initCell: function(x, y) {
+    this.cells[x + ',' + y] = {
+      x: x,
+      y: y,
+      neighbours: getNeighbours(x, y),
+      alive: true
+    };
+  },
+  addCell: function(x, y, alive) {
+    this.cells[x + ',' + y] = {
+      x: x,
+      y: y,
+      neighbours: getNeighbours(x, y),
+      alive: alive
+    };
+  },
+  cells: {},
+  nextTurn: function() {
+    for (var key in this.cells) {
+      this.cells[key].neighbours = getNeighbours(this.cells[key].x, this.cells[key].y);
+      addEmptyCells(this.cells[key].x, this.cells[key].y);
+    }
+  }
+};
+
+function getNeighbours(x, y) {
+  var neighbours = [];
+  for (var key in conway.cells) {
+    neighbours.push(key);
+  }
+  neighbours = neighbours.filter(function(element) {
+    var coordinates = element.split(',');
+    xRange = [x - 1, x, x + 1];
+    yRange = [y - 1, y, y + 1];
+    return conway.cells[element].alive && xRange.indexOf(+coordinates[0]) >= 0 && yRange.indexOf(+coordinates[1]) >= 0 && !(+coordinates[0] === x && +coordinates[1] === y);
+  });
+  return neighbours.length;
+}
+
+function range(start, end) {
+  var res = [];
+  for (var i = start; i <= end; i++) {
+    res.push(i);
+  }
+  return res;
+}
+
+function addEmptyCells(x, y) {
+  for (var i = x - 1; i <= x + 1; i++) {
+    for (var j = y - 1; j <= y + 1; j++) {
+      if (!conway.cells[i + ',' + j]){
+        conway.addCell(i, j, false);
+      }
+    }
+  }
+}
+
+module.exports = conway;
---
+++ test.js
@@ -0,0 +1,41 @@
+var test = require('tape');
+var conway = require('./conway');
+
+test("Initialising a cell adds it to the cells object", function(t) {
+  conway.initCell(0,0);
+  t.ok(conway.cells['0,0']);
+  conway.cells = [];
+  t.end();
+});
+
+test("Initialising a cell gets a count of its live neighbours", function(t) {
+  conway.initCell(0,0);
+  conway.initCell(0,1);
+  t.equal(conway.cells['0,1'].neighbours, 1);
+  conway.initCell(1,0);
+  t.equal(conway.cells['1,0'].neighbours, 2);
+  conway.cells = [];
+  t.end();
+});
+
+test("Each turn, a cell's live neighbour count is updated", function(t) {
+  conway.initCell(0,0);
+  t.equal(conway.cells['0,0'].neighbours, 0);
+  conway.initCell(0,1);
+  conway.nextTurn();
+  t.equal(conway.cells['0,0'].neighbours, 1);
+  conway.cells = [];
+  t.end();
+});
+
+test("Each turn, all empty cells with live neighbours are added to the cells " +
+"object", function(t) {
+  conway.initCell(0,0);
+  conway.nextTurn();
+  t.ok(conway.cells['0,1']);
+  t.ok(conway.cells['1,0']);
+  t.ok(conway.cells['1,1']);
+  t.equal(conway.cells['0,1'].alive, false);
+  conway.cells = [];
+  t.end();
+});
